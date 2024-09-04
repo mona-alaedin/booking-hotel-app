e831c04dbb25fef31792242c1a8eba70936952b4
alaedin <mona.alaedin1999@gmail.com>
2024-09-04 12:33:07 +0330

    useGeoLocation custom hook

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,6 +3,7 @@ import { useHotels } from "../context/HotelsProvider";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
+import useGeoLocation from "../Hooks/useGeoLocation";
 
 function Map() {
   const { isLoading, hotels } = useHotels();
@@ -10,10 +11,21 @@ function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    error,
+    getPosition,
+  } = useGeoLocation();
 
   useEffect(() => {
     if (lat && lng) setMapCenter([lat, lng]);
   }, [lat, lng]);
+
+  useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
 
   return (
     <div className="mapContainer">
@@ -22,6 +34,9 @@ function Map() {
         center={mapCenter}
         zoom={13}
         scrollWheelZoom={true}>
+        <button onClick={getPosition} className="getLocation">
+          {isLoadingPosition ? "Loading ..." : "Use Your Location"}
+        </button>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
